e99bfbb834a1a027b648cdd52efdf43b6c7f9e65
Zouinekh <zouinekhamine@gmail.com>
2023-12-12 00:54:12 +0100

    first commit

---
+++ src/main/java/com/example/app_notesv2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.app_notesv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MaterialButton addNoteBtn = findViewById(R.id.addnewnotebtn);
+
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AddNoteActivity.class));
+            }
+        });
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+       // MyAdapter myAdapter = new MyAdapter(getApplicationContext(),notesList);
+      //  recyclerView.setAdapter(myAdapter);
+
+
+
+    }
+}
